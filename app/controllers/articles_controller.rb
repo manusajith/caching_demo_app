37e9c610abd72e9aae55374f27d2e1cda2312c15
Manu S Ajith <neo@codingarena.in>
2013-04-30 03:54:07 +0530

    Adding page caching and action caching

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,6 +2,7 @@ class ArticlesController < ApplicationController
   before_filter :authenticate_user!
   # GET /articles
   # GET /articles.json
+  caches_action :index, :show
   def index
     @articles = Article.all
 
@@ -45,6 +46,7 @@ class ArticlesController < ApplicationController
 
     respond_to do |format|
       if @article.save
+        expire_action :action => [:index,:show]
         format.html { redirect_to @article, notice: 'Article was successfully created.' }
         format.json { render json: @article, status: :created, location: @article }
       else
@@ -61,6 +63,7 @@ class ArticlesController < ApplicationController
 
     respond_to do |format|
       if @article.update_attributes(params[:article])
+        expire_action :action => [:index,:show]
         format.html { redirect_to @article, notice: 'Article was successfully updated.' }
         format.json { head :no_content }
       else
@@ -75,6 +78,7 @@ class ArticlesController < ApplicationController
   def destroy
     @article = Article.find(params[:id])
     @article.destroy
+    expire_action :action => [:index,:show]
 
     respond_to do |format|
       format.html { redirect_to articles_url }
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
 	layout false
 
+	caches_page :index
   def index
   	redirect_to articles_path if user_signed_in?
   end
